30537609c70c2f3d9193af64570980af0b4a546a
nglaz <72976596+nglaz@users.noreply.github.com>
2020-10-28 09:36:04 +0200

    Update domzavd1M3.py
    Вибачте забувся, коментарі прибрав, це було для зручності перевірки коду

--- hw_lazuta/domzavd1M3.py
+++ hw_lazuta/domzavd1M3.py
@@ -4,15 +4,14 @@ name = str(input("Введіть своє ім'я: "))
 age = str(input("Вкажить свій вік: "))
 sex = str(input("Вкажить свою стать: "))
 
-# user_var1 = 'Hello! My name is '+name+". I'm "+age+". I'm
-# "+sex+'.'
-# print(user_var1)
+user_var1 = 'Hello! My name is '+name+". I'm "+age+". I'm "+sex+'.'
+print(user_var1)
 ####
-# user_var2 = "Hello! My name is %s. I'm %s. I'm %s." % (name, age, sex)
-# print(user_var2)
+user_var2 = "Hello! My name is %s. I'm %s. I'm %s." % (name, age, sex)
+print(user_var2)
 #####
-# user_var3 = "Hello! My name is {0}. I'm {1}. I'm {2}.".format(name, age, sex)
-# print(user_var3)
+user_var3 = "Hello! My name is {0}. I'm {1}. I'm {2}.".format(name, age, sex)
+print(user_var3)
 ####
 about_me_fstring = f"Hello! My name is {name}. I'm {age}. I'm {sex}."
 print(about_me_fstring)
@@ -22,17 +21,16 @@ txt2 = about_me_fstring[about_me_fstring.find('I'):about_me_fstring.rindex('I'):
 txt3 = about_me_fstring[about_me_fstring.rindex('I'):]
 print(txt1+'\n'+txt2+'\n'+txt3)
 ####
-# about_unname = about_me_fstring.replace("name is", 'friend name is').replace("I'm", "Нim", 2)
-# print(about_unname)
+about_unname = about_me_fstring.replace('Alex', 'Bogdan').replace("38", "28").replace('man', 'strong man')
+print(about_unname)
 ####
-# list_from_str = about_me_fstring.split('. ')
-# print(list_from_str)
+list_from_str = about_me_fstring.split('. ')
+print(list_from_str)
 ####
-# print(type(list_from_str))
+print(type(list_from_str))
 ####
-# str_from_list = '. '.join(list_from_str)
-# print(str_from_list)
+str_from_list = '. '.join(list_from_str)
+print(str_from_list)
 ####
-# about_me_fstring = about_me_fstring.swapcase()
-# print(about_me_fstring)
+print(about_me_fstring.swapcase())
 ####
