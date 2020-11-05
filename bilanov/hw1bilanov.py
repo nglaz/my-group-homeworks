fe62b1297d47d6a4a59e835ed1de0b5ef2f93379
karapuz228 <belanoff228@gmail.com>
2020-11-05 19:15:42 +0200

    hw1bilanov

--- bilanov/hw1bilanov.py
+++ bilanov/hw1bilanov.py
@@ -11,7 +11,15 @@ procent = 'Hello! My name is %s. I\'m %s, and i\'m a %s.' % (name, age, sex)
 print('% : ', procent)
 about_me_fstring = f'Hello! My name is {name}. I\'m {age}, and i\'m a {sex}.'
 print('f-string : ', about_me_fstring)
-'''named = 'Sasha'
-aged = '18'
-sexd = 'man'''
-
+str1 = about_me_fstring[:about_me_fstring.index('I')]
+str2 = about_me_fstring[about_me_fstring.find('I'):about_me_fstring.rindex('I'):]
+str3 = about_me_fstring[about_me_fstring.rindex('I'):]
+print(str1, '\n', str2, str3)
+about_friend_fstring = about_me_fstring.replace('Vanya', 'Masha').replace('20', '21').replace('man', 'woman')
+print(about_friend_fstring)
+list_from_str = about_me_fstring.split('. ')
+print(list_from_str)
+print(type(list_from_str))
+str_from_list = '. '.join(list_from_str)
+print(str_from_list)
+print(about_me_fstring.swapcase())
