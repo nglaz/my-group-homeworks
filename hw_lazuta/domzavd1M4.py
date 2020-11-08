e8575f33ee2e5a3ef96b4174108a2305785f1112
nglaz <nigretenok@gmail.com>
2020-11-08 14:44:27 +0200

    Newdone_domzavd1M4_81120

--- hw_lazuta/domzavd1M4.py
+++ hw_lazuta/domzavd1M4.py
@@ -11,31 +11,43 @@
 # регистрации, если не желает регистрироватся - пожелать удачи.
 # Данные о зарегестрированных пользователях хранить в файле 'users.txt',
 # по желанию можете создать файл для логирования событий регистрации и входа.
+import datetime
 
 zapros = input("Вітаємо на нашому ресурсі! Якщо ви зареєстровані напишить ("
-               "Y) інакше (N): ")
-with open('users.txt', 'r+', encoding='utf-8') as usfile:
+               "Y) інакше (N): ").upper()
+
+with open('users.txt', 'r+', encoding='utf-8') as usfile, \
+     open('loguser.txt', 'a+', encoding='utf-8') as logfile:
+
     logpas = str(usfile.readlines())
-    logpas = usfile.readlines()
-    #print(logpas)
+    now_datetime = datetime.datetime.today().strftime("%d.%m.%Y %H.%M.%S")
 
     if zapros == 'Y':
         login_user = input("Введіть свій логін: ")
         pass_user = input("Введіть свій пароль: ")
         if f"{login_user}:{pass_user}" in logpas:
             print(f"Вітаємо {login_user} на нашому ресурсі.")
+            logfile.write(f"{now_datetime} /{login_user}/-- вхід у ресурс\n")
         else:
             print("Невірно введений логін або пароль!")
+            logfile.write(f"{now_datetime} /{login_user}/-- невірний "
+                          f"логін або пароль\n")
     elif zapros == 'N':
         zapreg = input("Вітаємо! Бажаєте зареєструватися на нашому ресурсі ("
-        "введіть (Y), якщо ні то (N): ")
+        "введіть (Y), якщо ні то (N): ").upper()
         if zapreg == 'Y':
             login_new = input("Введіть логін: ")
             pass_new = input("Введіть пароль: ")
             if f"{login_new}:{pass_new}" not in logpas:
                 usfile.writelines(f"{login_new}:{pass_new}\n")
                 print(f"Вітаємо {login_new} на нашому ресурсі.")
+                logfile.write(f"{now_datetime} /{login_new}/-- нова "
+                              f"реєстрація у ресурсі\n")
             else:
                 print("Введений логін або пароль вже існує. Спробуйте ще!")
+                logfile.write(f"{now_datetime} /-- спроба зареєструватися "
+                              f"під існуючим логіном /{login_new}/\n")
         else:
             print("Дуже прикро! Всього Вам найкращого!")
+            logfile.write(f"{now_datetime} /-- користувач відхилив "
+                          f"реєстрацію у ресурсі\n")
