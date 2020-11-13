1e90adb22737d2e6cc6fbb96e1ebc3da984ee47e
vladpaliienko <paliienko.v@gmail.com>
2020-11-13 22:50:11 +0200

    deal

--- paliienko/hw4(2).py
+++ paliienko/hw4(2).py
@@ -1,23 +1,23 @@
-n = input("your favorite number")
 i = 0
 while True:
+    n = input("your favorite number")
     i = i + 1
-    if n.isdigit():
+    if n.isdigit() is True:
         print("good boy")
         break
-    elif n.isdigit() is False:
+    if n.isdigit() is False:
         if i == 1:
-            print(input("no problem, this time try number"))
+            print("no problem, this time try number")
         if i == 2:
-            print(input("come on i told you to print number"))
+            print("come on i told you to print number")
         if i == 3:
-            print(input("are you kidding me!!!!! look above it is"
-                        "NOT NUMBER!!! print pls number"))
+            print("are you kidding me!!!!! look above it is"
+                        "NOT NUMBER!!! print pls number")
         if i == 4:
-            print(input("OMG!!!!print number or i will find you ip"))
+            print("OMG!!!!print number or i will find you ip")
         if i == 5:
-            print(input("OMGOMG!!! you are not very clever, i know you ip"
-                        " already!!! its your last CHANCE! print number!!!!"))
+            print("OMGOMG!!! you are not very clever, i know you ip"
+                        " already!!! its your last CHANCE! print number!!!!")
         if i == 6:
             print("okay MTFCR, i am coming for you!! RUNNNNNNN!!!!! ")
             break
