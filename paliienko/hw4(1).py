301b9c3a248339d4cc8270196908f86ecb011497
vladpaliienko <paliienko.v@gmail.com>
2020-11-14 17:22:48 +0200

    hw4

--- paliienko/hw4(1).py
+++ paliienko/hw4(1).py
@@ -13,3 +13,15 @@ while index < quantity - 1:
         j += 1
     index += 1
 print(bubles)
+
+
+quantity = 10
+bubles = []
+for i in range(quantity):
+    bubles.append(randint(1, 99))
+print(bubles)
+for i in range(quantity-1):
+    for j in range(quantity-i-1):
+        if bubles[j] >  bubles[j+1]:
+            bubles[j], bubles[j+1] = bubles[j+1], bubles[j]
+print(bubles)
