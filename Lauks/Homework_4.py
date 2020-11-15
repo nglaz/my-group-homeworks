8c16aecba7173d245e0d3e18986d3570cfd55070
Andriy-Lauks <lva@mail.com>
2020-11-15 20:09:44 +0200

    Homework_4, 2-nd attempt

--- Lauks/Homework_4.py
+++ Lauks/Homework_4.py
@@ -1,7 +1,7 @@
 # Task 1
 lst = [9, 7, 6, 8, 4, 6, 5, 0]
 ind = 1
-while ind < len(lst) - 1:
+while ind <= len(lst) - 1:
     if lst[ind - 1] > lst[ind]:
         lst[ind], lst[ind - 1] = lst[ind - 1], lst[ind]
         ind = 1
@@ -10,22 +10,26 @@ while ind < len(lst) - 1:
         ind += 1
 print(lst)
 
+
 # Task 2
-fav_num = input('Please enter your favourite number')
-cnt = 1
-while fav_num.isdigit() is False:
+cnt = 0
+print('Please enter your favourite number')
+while True:
+    fav_num = input()
     cnt += 1
-    if cnt <= 3:
-        av_num = input('Please be more attentive, enter a "number"!')
-        continue
-    elif 3 < cnt <= 5:
-        fav_num = input('Enter NUMBER!!!')
-        continue
-    elif cnt == 6:
-        fav_num = input('It is your last chance!!!')
-        continue
+    if fav_num.isdigit() is False:
+        if cnt <= 3:
+            print ('Please be more attentive, enter a "number"!')
+            continue
+        elif 3 < cnt <= 5:
+            print('Enter a NUMBER!!!')
+            continue
+        elif cnt == 6:
+            print('It is your last chance!!!')
+            continue
+        else:
+            print('You are stupid mother fucker!!!')
+            break
     else:
-        print('You are stupid mother fucker!!!')
+        print('Great, thank you!')
         break
-else:
-    print('Great, thank you!')
