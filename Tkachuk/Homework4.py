9f233748ff7a615f6a4a4be74ba8acda82fe5fb6
Bogdan <b-tkachuk@ukr.net>
2020-11-17 15:52:58 +0200

    hw4_chenged

--- Tkachuk/Homework4.py
+++ Tkachuk/Homework4.py
@@ -1,19 +1,11 @@
 # Task 1
 from random import randint
-number_list = [randint(-100, 100) for i in range(5)]
-
+number_list = [randint(-100, 100) for i in range(10)]
 print(number_list)
-while True:
-    if number_list[0] > number_list[1]:
-        number_list[0], number_list[1] = number_list[1], number_list[0]
-    elif number_list[1] > number_list[2]:
-        number_list[1], number_list[2] = number_list[2], number_list[1]
-    elif number_list[2] > number_list[3]:
-        number_list[2], number_list[3] = number_list[3], number_list[2]
-    elif number_list[3] > number_list[4]:
-        number_list[3], number_list[4] = number_list[4], number_list[3]
-    else:
-        break
+for y in range(len(number_list)-1):
+    for x in range(len(number_list)-y-1):
+        if number_list[x] > number_list[x+1]:
+            number_list[x], number_list[x+1] = number_list[x+1], number_list[x]
 print(number_list)
 
 # Task 2
@@ -30,19 +22,17 @@ while True:
     elif count == 7 and favorite_num.isdigit() is False:
         print('You are an idiot! Have a nice day')
         break
-    elif count == 6 and favorite_num.isdigit() is False:
-        print('You have the last chance!')
-    elif count == 5 and favorite_num.isdigit() is False:
-        print('Numbers look like this -> 0 1 2 3 4 5 6 7 8 9')
-    elif count == 4 and favorite_num.isdigit() is False:
-        print('Are you serious?')
-    elif count == 3 and favorite_num.isdigit() is False:
-        print('Do you know what is it a number? '
-              'You entered the letter! Try more')
-    elif count == 2 and favorite_num.isdigit() is False:
-        print('It is not a number too,'
-              ' you should to think')
-    elif count == 1 and favorite_num.isdigit() is False:
+    elif count > 5 and favorite_num.isdigit() is False:
+        print('You have the last chance')
+    elif 3 < count <= 5 and favorite_num.isdigit() is False:
+        print('It is not a number too, you should to think')
+    elif count <= 3 and favorite_num.isdigit() is False:
         print('It is not a number!')
+
+
+
+
+
+
 
 
