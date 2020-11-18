b7861fb7405eb0bd0047863d5dd05d59badf33d5
Valya <Valtltltl@gmail.com>
2020-11-18 02:05:44 +0200

    Yakymets

--- yakymets/HW4(1).py
+++ yakymets/HW4(1).py
@@ -2,15 +2,15 @@ from random import randint
 
 V = 13
 a = []
-def bubble(list):
-    for i in range(V-1):
-        for j in range(V-i-1):
-            if list[j] > list[j +1]:
-                list[j] , list[j + 1] = list[j + 1] , list[j]
-
 for i in range(V):
     a.append(randint(1, 99))
-
 print(a)
-bubble(a)
+
+for i in range(V-1):
+    for r in range(V-i-1):
+        if a[r] > a[r +1]:
+             a[r] , a[r + 1] = a[r + 1] , a[r]
+
+
+
 print(a)
--- yakymets/HomeWork4(2).py
+++ yakymets/HomeWork4(2).py
@@ -2,23 +2,17 @@ v = 0
 while True :
     words = input(" Введите своё любимое число ")
     v += 1
-    if v <= 3:
-        if words.isdigit() == True:
-            print(" Спасибо ")
-            break
-        if words.isdigit() == False:
-            print("Неверно, попробуйте ещё раз.")
-    elif 3 < v <= 5:
-        if words.isdigit() == True:
-            print(" Спасибо ")
-            break
-        if words.isdigit() == False:
-            print(" Внимательней ! Нужно ввести именно число ! ")
-    elif 5 < v <= 6:
-         input(" Если не сейчас , то никогда . Введите число !")
-         if words.isdigit() == True:
-            print(" Ура ! Хорошего Вам дня.")
-            break
-         if words.isdigit() == False:
-            print(" Никогда сюда не возвращайтесь! ")
-            break
+    if  words.isdigit() == True:
+        print(" Спасибо ")
+        break
+    elif words.isdigit() == False:
+        if v<=3 :
+             print("Ошибка, попробуйте ещё раз")
+        if 3<v<5 :
+             print("Нужно быть внимательнее")
+        if 4<v<6 :
+            print("Нужно быть внимательнее")
+            print("Остался последний шанс")
+        if 5<v<=6:
+             print("Не возвращайтесь сюда никогда")
+             break
