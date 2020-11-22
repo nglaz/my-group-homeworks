90a4a8bbb18faca3612497ee5fb15577917600c0
karapuz228 <belanoff228@gmail.com>
2020-11-22 19:06:21 +0200

    hw5 reloaded

--- bilanov/hw5.py
+++ bilanov/hw5.py
@@ -11,11 +11,11 @@ while answer != 'en' and answer != 'de':
     answer = input('please type "en" or "de" to encrypt or decrypt\n')
 
 while True:
-    key = input('enter a key in range 1-25: ')
-    if key.isdigit() is False:
+    key1 = input('enter a key in range 1-25: ')
+    if key1.isdigit() is False:
         print('please enter a NUMBER')
     else:
-        if int(key) > 25 or int(key) < 1:
+        if int(key1) > 25 or int(key1) < 1:
             print('please enter a valid number')
         else:
             print('key correct. continuing...')
@@ -23,66 +23,70 @@ while True:
 
 
 alphabet = string.ascii_letters
+digits = string.digits + string.digits
 
 
 def encrypt(key=1):
-    fileToEnCrypt = input('enter a file name you want to encrypt\n')
-    fileEncrypted = input('enter a file name you want to save your encrypted '
-                          'data\n')
-    with open(f'{fileToEnCrypt}.txt', 'a+', encoding='utf-8') as fencrypt,\
-        open(f'{fileEncrypted}.txt', 'a+', encoding='utf-8') as fencrypted:
-        fencrypt.seek(0)
-        tocrypt = fencrypt.read()
-        for letter in tocrypt:
-            position = alphabet.find(letter)
-            newPosition = position + int(key)
-            if letter in alphabet:
-                fencrypted.write(alphabet[newPosition])
-            else:
-                fencrypted.write(letter)
-    print(f'Your {fileToEnCrypt}.txt file has been encrypted and saved as'
-          f' {fileEncrypted}.txt . Thanks for using our program)')
+    file_to_encrypt = input('enter a file name you want to encrypt\n')
+    file_encrypted = input('enter a file name you want to save your '
+                           'encrypted data\n')
+    try:
+        with open(f'{file_to_encrypt}.txt', 'r+', encoding='utf-8') as \
+                fencrypt, open(f'{file_encrypted}.txt', 'a+',
+                               encoding='utf-8') as fencrypted:
+            fencrypt.seek(0)
+            tocrypt = fencrypt.read()
+            for letter in tocrypt:
+                position = alphabet.find(letter)
+                newposition = position + int(key)
+                if letter in alphabet:
+                    fencrypted.write(alphabet[newposition])
+                elif letter in digits:
+                    dposition = digits.find(letter)
+                    newdposition = dposition + int(key)
+                    fencrypted.write(digits[newdposition])
+                else:
+                    fencrypted.write(letter)
+        print(f'Your {file_to_encrypt}.txt file has been encrypted and saved '
+              f'as {file_encrypted}.txt . Thanks for using our program)')
+    except FileNotFoundError:
+        print(f'File {file_to_encrypt}.txt not found. Try again...')
 
 
 def decrypt(key=1):
-    fileToDeCrypt = input('enter a file name you want to decrypt\n')
-    fileDecrypted = input('enter a file name you want to save your decrypted '
-                          'data\n')
-    with open(f'{fileToDeCrypt}.txt', 'a+', encoding='utf-8') as fdecrypt,\
-        open(f'{fileDecrypted}.txt', 'a+', encoding='utf-8') as fdecrypted:
-        fdecrypt.seek(0)
-        todecrypt = fdecrypt.read()
-        for letter in todecrypt:
-            position = alphabet.find(letter)
-            newPosition = position - int(key)
-            if letter in alphabet:
-                fdecrypted.write(alphabet[newPosition])
-            else:
-                fdecrypted.write(letter)
-    print(f'Your {fileToDeCrypt}.txt file has been encrypted and saved as'
-          f' {fileDecrypted}.txt . Thanks for using our program)')
-
+    file_to_decrypt = input('enter a file name you want to decrypt\n')
+    file_decrypted = input('enter a file name you want to save your decrypted '
+                           'data\n')
+    try:
+        with open(f'{file_to_decrypt}.txt', 'r+', encoding='utf-8') as \
+                fdecrypt, open(f'{file_decrypted}.txt', 'a+',
+                               encoding='utf-8') as fdecrypted:
+            fdecrypt.seek(0)
+            todecrypt = fdecrypt.read()
+            for letter in todecrypt:
+                position = alphabet.find(letter)
+                newposition = position - int(key)
+                if letter in alphabet:
+                    fdecrypted.write(alphabet[newposition])
+                elif letter in digits:
+                    dposition = digits.find(letter)
+                    newdposition = dposition - int(key)
+                    fdecrypted.write(digits[newdposition])
+                else:
+                    fdecrypted.write(letter)
+        print(f'Your {file_to_decrypt}.txt file has been encrypted and saved '
+              f'as {file_decrypted}.txt . Thanks for using our program)')
+    except FileNotFoundError:
+        print(f'File {file_to_decrypt}.txt not found. Try again...')
 
 
 # Если зашифровать, попросить у него ввести имя шифруемого файла, имя файла,
 # который мы получим на выходе(зашифрованного) и
 # ключ(ключем будет уроваень сдвига).
 if answer == 'en':
-    encrypt(key)
+    encrypt(int(key1))
 
 # Если расшифровать, попросить ввести имя зашифрованного файла, имя файла на
 # выходе(расшифрованного) и ключ.
 else:
-    decrypt(key)
-
-
-
-
-
-
-
-
-
-
-
-
+    decrypt(int(key1))
