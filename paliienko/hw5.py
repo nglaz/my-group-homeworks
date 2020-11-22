fe59282da39b393930ca2f0f9e87b7e1deb5b333
vladpaliienko <paliienko.v@gmail.com>
2020-11-22 21:20:13 +0200

    hw5

--- paliienko/hw5.py
+++ paliienko/hw5.py
@@ -1,9 +1,29 @@
-def encrypt():
+def encrypt(key, file, fileen):
+    with open(file, "r", encoding="utf-8") as crypto:
+        crypto.seek(0)
+        crp=crypto.read()
+        encrypted = ''.join(chr(ord(ceaser) + key) for ceaser in crp)
+    with open (fileen, "w+", encoding="utf-8") as crypted:
+        crypted.write(encrypted)
+    print("encrypted result in encp file")
+
+def decrypt(key, filede, filedecr):
+    with open(filede, "r", encoding="utf-8") as decrypto:
+        decrypto.seek(0)
+        decrp=decrypto.read()
+        deencrypted = ''.join(chr(ord(ceaser) - key) for ceaser in decrp)
+    with open (filedecr, "w+", encoding="utf-8") as decrypted:
+        decrypted.write(deencrypted)
+    print("decryption result in inputed decp file")
+
+
+main = input("encrypt or decrypt? e = encrypt, d = decrypt")
+if main == "e":
     while True:
         try:
-           key = int(input("pls input key from 1 to 25"))
+            key = int(input("pls input key from 1 to 25"))
         except ValueError:
-           print("try number pls")
+            print("try number pls")
         else:
             break
     while True:
@@ -17,17 +37,9 @@ def encrypt():
             print("There is no such file in directory")
         else:
             break
-    with open(file, "r", encoding="utf-8") as crypto:
-        crypto.seek(0)
-        crp=crypto.read()
-        encrypted = ''.join(chr(ord(ceaser) + key) for ceaser in crp)
     fileen = input("input name encp file")
-    with open (fileen, "w+", encoding="utf-8") as crypted:
-        crypted.write(encrypted)
-    print("encrypted result in encp file")
-
-
-def decrypt():
+    encrypt(key,file,fileen)
+elif main == "d":
     while True:
         try:
             key = int(input("pls input key from 1 to 25"))
@@ -46,18 +58,5 @@ def decrypt():
             print("There is no such file in directory")
         else:
             break
-    with open(filede, "r", encoding="utf-8") as decrypto:
-        decrypto.seek(0)
-        decrp=decrypto.read()
-        deencrypted = ''.join(chr(ord(ceaser) - key) for ceaser in decrp)
     filedecr = input("input name decp file")
-    with open (filedecr, "w+", encoding="utf-8") as decrypted:
-        decrypted.write(deencrypted)
-    print("decryption result in inputed decp file")
-
-
-main = input("encrypt or decrypt? e = encrypt, d = decrypt")
-if main == "e":
-    encrypt()
-elif main == "d":
-    decrypt()
+    decrypt(key,filede,filedecr)
