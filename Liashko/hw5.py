92da503e774ccbf134c682ace98f93a6e875c8f3
Dashulyazt <dashulyazt@gmail.com>
2020-11-23 19:32:39 +0200

    HW#5+

--- Liashko/hw5.py
+++ Liashko/hw5.py
@@ -1,5 +1,6 @@
 import string
-shfr = input('Вы хотите зашифровать(Z) либо расшифровать(R)')
+
+shfr = input('Вы хотите зашифровать(Z) либо расшифровать(R)').upper()
 
 
 def cezar_shifr(lines_file):
@@ -26,19 +27,38 @@ def cezar_shifr(lines_file):
     return shifrs
 
 
-if shfr == 'Z':
-    name_file = input('Введите имя файла для его зашифровки')
-    name_file_z = input('Введите имя зафрованного файла')
-    key_z = int(input('Ключ сдвига'))
-    with open(name_file + '.txt') as file:
-        lines_file = file.readlines()
-    with open(name_file_z + '.txt', 'w+') as file:
-        file.write(cezar_shifr(lines_file))
-else:
-    name_file_roz = input('Введите имя файла для расшифровки')
-    name_file_a = input('Введите имя файла после его расшифровки')
-    key_z = input('Ключ сдвига')
-    with open(name_file_roz + '.txt') as file:
-        lines_file = file.readlines()
-    with open(name_file_a + '.txt', 'w+') as file:
-        file.write(cezar_shifr(lines_file))
+while True:
+    try:
+        if shfr == 'Z':
+            try:
+                name_file = input('Введите имя файла для его зашифровки')
+                with open(name_file + '.txt') as file:
+                    lines_file = file.readlines()
+            except FileNotFoundError:
+                print('Ошибка: Просьба ввести существующий файл для шифровки')
+                break
+            name_file_z = input('Введите имя зафрованного файла')
+            key_z = int(input('Ключ сдвига'))
+            with open(name_file_z + '.txt', 'w+') as file:
+                file.write(cezar_shifr(lines_file))
+        elif shfr == 'R':
+            try:
+                name_file_roz = input('Введите имя файла для расшифровки')
+                with open(name_file_roz + '.txt') as file:
+                    lines_file = file.readlines()
+            except FileNotFoundError:
+                print('Ошибка: Просьба ввести существующий файл для шифровки')
+                break
+            name_file_a = input('Введите имя файла после его расшифровки')
+            key_z = int(input('Ключ сдвига'))
+            # изменение для дешифратора
+            key_z = - key_z
+            with open(name_file_a + '.txt', 'w+') as file:
+                file.write(cezar_shifr(lines_file))
+        else:
+            print('Ошибка: Введите верное значение')
+            break
+    except ValueError:
+        print('Ошибка: Просьба ввести цифру')
+        break
+    break
