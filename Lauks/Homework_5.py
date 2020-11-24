e9bef96b1ff30f7030410c075a46be33f9c38d87
Andriy-Lauks <lva@mail.com>
2020-11-24 17:22:19 +0200

    Homework 5, attempt 3

--- Lauks/Homework_5.py
+++ Lauks/Homework_5.py
@@ -2,24 +2,22 @@
 
 import string
 
-sample = string.printable
-
-def encrypt(*args):
-    output = ''.join(list(map(lambda letter: sample[sample.index(letter) - int(key)], content)))
-    return output
-
-
-def decrypt(*args):
-    output = ''.join(list(map(lambda letter: sample[sample.index(letter) + int(key)  - len(sample)],\
-                              content)))
-    return output
-
 
 def processing(*args):
     with open(source, 'r+', encoding='UTF8') as file:
-        global content
         content = file.read()
         file.close()
+
+    sample = string.printable
+
+    def encrypt(*args):
+        output = ''.join(list(map(lambda letter: sample[sample.index(letter) - int(key)], content)))
+        return output
+
+    def decrypt(*args):
+        output = ''.join(list(map(lambda letter: sample[sample.index(letter) + int(key) - len(sample)], content)))
+        return output
+
     with open(result, 'w+', encoding='UTF8') as file:
         if process == 'encrypt':
             print(encrypt(content), file=file)
@@ -29,18 +27,11 @@ def processing(*args):
     return
 
 
-def user_input():
-    global process
-    process = input('Type "encrypt" if you wish to encrypt your file or "decrypt" if '
-        'you wish to decrypt your file.').lower().strip()
-    if process == 'encrypt' or process == 'decrypt':
-        global source, result, key
-        source, result, key = input('''Please enter: \nsource file name, 
-result file name \nand the key, \nall separated with "space".''').split()
-        processing()
-    else:
-        print('Wrong answer')
-        user_input()
-    return process, source, result, key
-
-user_input()
+process = input('Type "encrypt" if you wish to encrypt your file or "decrypt" '
+                'if you wish to decrypt your file.').lower().strip()
+if process == 'encrypt' or process == 'decrypt':
+    source, result, key = input('Please enter: \nsource file name, result file name \nand the key, '
+                                '\nall separated with "space".').split()
+    processing()
+else:
+    print('Wrong answer')
