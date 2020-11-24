adf44a8f63cd560c37692320fb9963b7805ff279
Andriy-Lauks <lva@mail.com>
2020-11-24 09:53:24 +0200

    Homework 5, attepmt 2

--- Lauks/Homework_5.py
+++ Lauks/Homework_5.py
@@ -2,39 +2,45 @@
 
 import string
 
-source = string.printable
+sample = string.printable
 
-def encrypt(*kwargs):
-    output = ''.join(list(map(lambda letter: source[source.index(letter) - int(key)], content)))
+def encrypt(*args):
+    output = ''.join(list(map(lambda letter: sample[sample.index(letter) - int(key)], content)))
     return output
 
 
-def decrypt(*kwargs):
-    output = ''.join(list(map(lambda letter: source[source.index(letter) + int(key)  - len(source)],\
+def decrypt(*args):
+    output = ''.join(list(map(lambda letter: sample[sample.index(letter) + int(key)  - len(sample)],\
                               content)))
     return output
 
-choice = input('Type "encrypt" if you wish to encrypt your file or "decrypt" if '
-               'you wish to decrypt your file.').lower().strip()
 
-if choice == 'encrypt':
-    input_file, output_file, key = input('''Please enter: \ndecrypted file name,
-encrypted file name \nand the key, \nall separated with "space".''').split()
-    with open(input_file, 'r+', encoding='UTF8') as file:
+def processing(*args):
+    with open(source, 'r+', encoding='UTF8') as file:
+        global content
         content = file.read()
         file.close()
-    with open(output_file, 'w+', encoding='UTF8') as file:
-        file.write(encrypt(content))
+    with open(result, 'w+', encoding='UTF8') as file:
+        if process == 'encrypt':
+            print(encrypt(content), file=file)
+        else:
+            print(decrypt(content), file=file)
         file.close()
+    return
 
-elif choice == 'decrypt':
-    input_file, output_file, key = input('''Please enter: \nencrypted file name, 
-decrypted file name \nand the key, \n all separated with "space".''').split()
-    with open(input_file, 'r+', encoding='UTF8') as file:
-        content = file.read()
-        file.close()
-    with open(output_file, 'w+', encoding='UTF8') as file:
-        file.write(decrypt(content))
-        file.close()
-else:
-    print('Wrong answer')
+
+def user_input():
+    global process
+    process = input('Type "encrypt" if you wish to encrypt your file or "decrypt" if '
+        'you wish to decrypt your file.').lower().strip()
+    if process == 'encrypt' or process == 'decrypt':
+        global source, result, key
+        source, result, key = input('''Please enter: \nsource file name, 
+result file name \nand the key, \nall separated with "space".''').split()
+        processing()
+    else:
+        print('Wrong answer')
+        user_input()
+    return process, source, result, key
+
+user_input()
