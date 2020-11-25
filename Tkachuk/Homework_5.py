0cf08deab667524740b871bbe2882f586d4207d0
Bogdan <b-tkachuk@ukr.net>
2020-11-25 11:30:28 +0200

    hw5_changed#3

--- Tkachuk/Homework_5.py
+++ Tkachuk/Homework_5.py
@@ -1,54 +1,63 @@
 import string
-
-alphabet = string.ascii_lowercase * 2
-numbers = string.digits * 2
 choice = int(input('Please enter 1 if you want to encrypt'
                    ' or 2 if you want to decrypt '))
-if choice == 1:
-    def encryption(text_file, key, encryption_file):
-        result = ''
-        with open(text_file) as file:
-            text = file.read().lower()
-        for symbol in text:
-            new_position = alphabet.find(symbol) + key
-            new_position_num = numbers.find(symbol) + key
-            if symbol in alphabet:
-                result = result + alphabet[new_position]
-            elif symbol in numbers:
-                result = result + numbers[new_position_num]
-            else:
-                result = result + symbol
+
+
+def encryption(text_file, key, encryption_file):
+    result = ''
+    with open(text_file) as file:
+        text = file.read().lower()
+    for item in text:
+        alphabet = string.ascii_lowercase * (key + 1)
+        numbers = string.digits * (key + 1)
+        symbols = string.punctuation * (key + 1)
+        new_position = alphabet.find(item) + key
+        new_position_num = numbers.find(item) + key
+        new_position_sym = symbols.find(item) + key
+        if item in alphabet:
+            result = result + alphabet[new_position]
+        elif item in numbers:
+            result = result + numbers[new_position_num]
+        elif item in symbols:
+            result = result + symbols[new_position_sym]
+        else:
+            result = result + item
 
         with open(encryption_file, 'w') as en_file:
             en_file.write(result)
 
 
-    encryption(
-        text_file=input('Please enter name of your file '),
-        key=int(input('Please enter encryption key ')),
-        encryption_file=input('Please enter name of encryption file ')
-    )
-
-
-elif choice == 2:
-    def decryption(encryption_file, key, decryption_file):
-        result = ''
-        with open(encryption_file) as file:
-            text = file.read().lower()
-        for symbol in text:
-            new_position = alphabet.find(symbol) - key
-            new_position_num = numbers.find(symbol) - key
-            if symbol in alphabet:
-                result = result + alphabet[new_position]
-            elif symbol in numbers:
-                result = result + numbers[new_position_num]
-            else:
-                result = result + symbol
+def decryption(encryption_file, key, decryption_file):
+    result = ''
+    with open(encryption_file) as file:
+        text = file.read().lower()
+    for item in text:
+        alphabet = string.ascii_lowercase * (key + 1)
+        numbers = string.digits * (key + 1)
+        symbols = string.punctuation * (key + 1)
+        new_position = alphabet.find(item) - key
+        new_position_num = numbers.find(item) - key
+        new_position_sym = symbols.find(item) - key
+        if item in alphabet:
+            result = result + alphabet[new_position]
+        elif item in numbers:
+            result = result + numbers[new_position_num]
+        elif item in symbols:
+            result = result + symbols[new_position_sym]
+        else:
+            result = result + item
 
         with open(decryption_file, 'w') as file:
             file.write(result)
 
 
+if choice == 1:
+    encryption(
+        text_file=input('Please enter name of your file '),
+        key=int(input('Please enter encryption key ')),
+        encryption_file=input('Please enter name of encryption file ')
+    )
+elif choice == 2:
     decryption(
         encryption_file=input('Please enter name of encryption file '),
         key=int(input('Please enter decryption key ')),
@@ -57,3 +66,70 @@ elif choice == 2:
 
 
 
+# import string
+#
+# alphabet = string.ascii_lowercase * 2
+# numbers = string.digits * 2
+# symbols = string.punctuation * 2
+# choice = int(input('Please enter 1 if you want to encrypt'
+#                    ' or 2 if you want to decrypt '))
+#
+#
+# def encryption(text_file, key, encryption_file):
+#     result = ''
+#     with open(text_file) as file:
+#         text = file.read().lower()
+#     for item in text:
+#         new_position = alphabet.find(item) + key
+#         new_position_num = numbers.find(item) + key
+#         new_position_sym = symbols.find(item) + key
+#         if item in alphabet:
+#             result = result + alphabet[new_position]
+#         elif item in numbers:
+#             result = result + numbers[new_position_num]
+#         elif item in symbols:
+#             result = result + symbols[new_position_sym]
+#         else:
+#             result = result + item
+#
+#         with open(encryption_file, 'w') as en_file:
+#             en_file.write(result)
+#
+#
+# def decryption(encryption_file, key, decryption_file):
+#     result = ''
+#     with open(encryption_file) as file:
+#         text = file.read().lower()
+#     for item in text:
+#         new_position = alphabet.find(item) - key
+#         new_position_num = numbers.find(item) - key
+#         new_position_sym = symbols.find(item) - key
+#         if item in alphabet:
+#             result = result + alphabet[new_position]
+#         elif item in numbers:
+#             result = result + numbers[new_position_num]
+#         elif item in symbols:
+#             result = result + symbols[new_position_sym]
+#         else:
+#             result = result + item
+#
+#         with open(decryption_file, 'w') as file:
+#             file.write(result)
+#
+# if choice == 1:
+#     encryption(
+#         text_file=input('Please enter name of your file '),
+#         key=int(input('Please enter encryption key ')),
+#         encryption_file=input('Please enter name of encryption file ')
+#     )
+# elif choice == 2:
+#     decryption(
+#         encryption_file=input('Please enter name of encryption file '),
+#         key=int(input('Please enter decryption key ')),
+#         decryption_file=input('Please enter name of decryption file ')
+#     )
+#
+#
+#
+
+#
