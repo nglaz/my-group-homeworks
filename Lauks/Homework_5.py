f87c90d1f8ab60c06710050bb2c427460cd5a64a
Andriy-Lauks <lva@mail.com>
2020-11-25 20:20:42 +0200

    Homework 5, attempt 4

--- Lauks/Homework_5.py
+++ Lauks/Homework_5.py
@@ -1,37 +1,68 @@
-# Task "Caesar's Cypher"
-
 import string
-
-
-def processing(*args):
-    with open(source, 'r+', encoding='UTF8') as file:
-        content = file.read()
-        file.close()
-
-    sample = string.printable
-
-    def encrypt(*args):
-        output = ''.join(list(map(lambda letter: sample[sample.index(letter) - int(key)], content)))
-        return output
-
-    def decrypt(*args):
-        output = ''.join(list(map(lambda letter: sample[sample.index(letter) + int(key) - len(sample)], content)))
-        return output
-
-    with open(result, 'w+', encoding='UTF8') as file:
-        if process == 'encrypt':
-            print(encrypt(content), file=file)
-        else:
-            print(decrypt(content), file=file)
-        file.close()
-    return
-
 
 process = input('Type "encrypt" if you wish to encrypt your file or "decrypt" '
                 'if you wish to decrypt your file.').lower().strip()
 if process == 'encrypt' or process == 'decrypt':
-    source, result, key = input('Please enter: \nsource file name, result file name \nand the key, '
-                                '\nall separated with "space".').split()
-    processing()
+    pass
 else:
-    print('Wrong answer')
+    print('You have entered wrong option, please check and run the program again')
+    exit()
+
+def source():
+    source = input('Please enter source file name') + '.txt'
+
+    def file_read(*args):
+        try:
+            with open(source, 'r+', encoding='UTF8') as file:
+                content = file.read()
+                file.close()
+        except FileNotFoundError:
+            print('No such source file, please check and run the program again')
+            exit()
+        return content
+
+    def result():
+        result = input('Please enter result file name') + '.txt'
+
+        return result
+
+    def file_write(*args):
+
+        def key():
+            key = input('Please enter a number for encryption key')
+            try:
+                key = int(key)
+            except ValueError:
+                while key.isdigit() == False:
+                    key = input('You entered not a valid number, please try again')
+            key = int(key)
+            while key >= len(sample):
+                key = int(input('The key you entered is too big, please enter smaller one'))
+
+            return key
+
+        sample = string.ascii_letters
+
+        def encrypt(*args):
+            output = ''.join(list(map(lambda letter: sample[sample.index(letter) - key]
+            if letter in sample else letter, file_read())))
+            return output
+
+        def decrypt(*args):
+            output = ''.join(list(map(lambda letter: sample[sample.index(letter)
+            + key - len(sample)] if letter in sample else letter, file_read())))
+            return output
+
+        with open(result(), 'w+', encoding='UTF8') as file:
+            key = key()
+            if process == 'encrypt':
+                print(encrypt(), file=file)
+            elif process == 'decrypt':
+                print(decrypt(), file=file)
+            file.close()
+
+    file_write()
+    return source
+
+
+source()
