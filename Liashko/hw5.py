33e77243ab8be4c71b00cea2fc867bf6ab6b56c3
Dashulyazt <dashulyazt@gmail.com>
2020-11-26 18:02:35 +0200

    Explode on func

--- Liashko/hw5.py
+++ Liashko/hw5.py
@@ -3,7 +3,38 @@ import string
 shfr = input('Вы хотите зашифровать(Z) либо расшифровать(R)').upper()
 
 
-def caesar_shifr(lines_file):
+def file_for_work():
+    while True:
+        try:
+            name_file = input('Введите имя исходного файла')
+            with open(name_file + '.txt') as file:
+                lines_file = file.readlines()
+            return lines_file
+        except FileNotFoundError:
+            print('Ошибка: Просьба ввести существующий файл')
+            break
+
+
+def work_with_ans(shfr):
+    while True:
+        try:
+            if shfr == 'Z':
+                key_z = int(input('Ключ сдвига'))
+            elif shfr == 'R':
+                key_z = int(input('Ключ сдвига'))
+                key_z = - key_z
+            else:
+                print('Ошибка: Введите верное значение')
+                break
+        except ValueError:
+            print('Ошибка: Просьба ввести цифру')
+            break
+        return key_z
+
+
+def caesar_shifr():
+    lines_file = file_for_work()
+    key_z = work_with_ans(shfr)
     shifr_after = []
     for i in range(len(lines_file[0])):
         # проверка буква ли это
@@ -24,41 +55,10 @@ def caesar_shifr(lines_file):
             g = lines_file[0][i]
         shifr_after.append(g)
     shifrs = ''.join(shifr_after)
-    return shifrs
+    name_file_z = input('Введите имя файла для вывода результата')
+    with open(name_file_z + '.txt', 'w+') as file:
+        file.write(shifrs)
+    return file
 
 
-while True:
-    try:
-        if shfr == 'Z':
-            try:
-                name_file = input('Введите имя файла для его зашифровки')
-                with open(name_file + '.txt') as file:
-                    lines_file = file.readlines()
-            except FileNotFoundError:
-                print('Ошибка: Просьба ввести существующий файл для шифровки')
-                break
-            name_file_z = input('Введите имя зашифрованного файла')
-            key_z = int(input('Ключ сдвига'))
-            with open(name_file_z + '.txt', 'w+') as file:
-                file.write(caesar_shifr(lines_file))
-        elif shfr == 'R':
-            try:
-                name_file_roz = input('Введите имя файла для расшифровки')
-                with open(name_file_roz + '.txt') as file:
-                    lines_file = file.readlines()
-            except FileNotFoundError:
-                print('Ошибка: Просьба ввести существующий файл для шифровки')
-                break
-            name_file_a = input('Введите имя файла после его расшифровки')
-            key_z = int(input('Ключ сдвига'))
-            # изменение для дешифратора, чтобы работало в обратном порядке
-            key_z = - key_z
-            with open(name_file_a + '.txt', 'w+') as file:
-                file.write(caesar_shifr(lines_file))
-        else:
-            print('Ошибка: Введите верное значение')
-            break
-    except ValueError:
-        print('Ошибка: Просьба ввести цифру')
-        break
-    break
+caesar_shifr()
